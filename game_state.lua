4dc4fe4330aa2cdc75139c6dd2b4cf89dc4f48fb
Hawker <kovujacksonbentley@gmail.com>
2026-05-18 16:22:05 -0400

    get player hand cards
    Only includes base player cards when starting / loading into a round, and discard / playing cards

---
+++ game_state.lua
@@ -0,0 +1,81 @@
+-- todo either find or make a sort function for playing cards
+-- Class to hold the state that will be transferred to the calculator
+local GameState = {}
+GameState.__index = GameState
+
+function GameState.new()
+    local self = setmetatable({}, GameState)
+    self.playing_cards = {} -- the playing cards in hand during a blind
+    return self
+end
+
+-- Overwrite the playing cards
+function GameState:set_playing_cards(cards)
+    for _, card in ipairs(cards) do
+        table.insert(self.playing_cards, card)
+    end
+end
+
+-- Add cards to playing cards
+function GameState:add_playing_cards(cards)
+    for _, card in ipairs(cards) do
+        table.insert(self.playing_cards, card)
+    end
+end
+
+-- Remove cards from playing cards
+function GameState:remove_playing_cards(cards_to_remove)
+    local new_playing_cards = {}
+
+    for _, card in ipairs(self.playing_cards) do
+        local should_remove = false
+
+        -- Check if this card matches any card to remove
+        for _, target_card in ipairs(cards_to_remove) do
+            if same_playing_card(card, target_card) then
+                should_remove = true
+                break
+            end
+        end
+
+        -- Keep card if it should not be removed
+        if not should_remove then
+            table.insert(new_playing_cards, card)
+        end
+    end
+
+    self.playing_cards = new_playing_cards
+end
+
+-- Print the playing cards
+function GameState:print_playing_cards()
+    print("Playing cards in hand: " .. print_play_card_data(self.playing_cards))
+end
+
+-- Helper function to see if two playing cards are the same
+function same_playing_card(c1, c2)
+    -- todo need to add seals, enhancements, and editions
+    return c1.base.id == c2.base.id and 
+           c1.base.suit == c2.base.suit
+end
+
+-- Helper function to print playing cards (not necessarily the ones in the player's hand)
+function print_play_card_data(cards)
+    local card_strings = {}
+    local card_names = {
+        [11] = "Jack",
+        [12] = "Queen",
+        [13] = "King",
+        [14] = "Ace"
+    }
+
+    for _, card in ipairs(cards) do
+        local id = card.base.id
+        local name = card_names[id] or tostring(id)
+        table.insert(card_strings, name .. " of " .. card.base.suit)
+    end
+
+    return table.concat(card_strings, ", ") .. " (" .. #cards .. ")"
+end
+
+return GameState
--- main.lua
+++ main.lua
@@ -1,1 +1,113 @@
 print("Card Parser mod loaded")
+
+-- variable to hold data for transfer to calculator
+local GameState = assert(SMODS.load_file('game_state.lua'))()
+local game_state = GameState.new()
+
+SMODS.current_mod.calculate = function(self, context)
+    -- Get the playing cards the user got in hand when starting a blind
+    -- if the user has not used a hand yet, overwrite the playing cards
+    -- otherwise, append the new cards to the playing cards
+    if context.hand_drawn or context.other_drawn then
+        local first_hand_drawn = context.first_hand_drawn
+        local cards = context.hand_drawn or context.other_drawn
+        G.E_MANAGER:add_event(Event({
+            func = function()
+                if first_hand_drawn then
+                    game_state:set_playing_cards(cards)
+                else
+                    game_state:add_playing_cards(cards)
+                end
+                game_state:print_playing_cards()
+                return true
+            end
+        }))
+    end
+
+    -- remove playing cards when they are used in a hand
+    if context.press_play then
+        local cards = G.hand.highlighted
+        game_state:remove_playing_cards(cards)
+    end
+
+    -- remove playing cards when they are used in a discard
+    if context.pre_discard then
+        local cards = context.full_hand
+        G.E_MANAGER:add_event(Event({
+            func = function()
+                    game_state:remove_playing_cards(cards)
+                return true
+            end
+        }))
+    end
+end
+
+local game_start_run_ref = Game.start_run
+function Game:start_run(args, ...)
+    -- get the playing cards the user got in hand when loading into a round
+    -- verify this only happen if the user is in a blind
+    local ret = game_start_run_ref(self, args, ...)
+    if args.savetext then
+        local blind = G.GAME.blind.config.blind.key
+        local cards = G.hand.cards;
+        G.E_MANAGER:add_event(Event({
+            func = function()
+                if blind ~= nil then
+                    game_state:set_playing_cards(cards)
+                    game_state:print_playing_cards()
+                end
+                return true
+            end
+        }))
+    end
+    return ret
+end
+
+-- G.hand.cards
+    -- Check for the following states
+        -- Consumables
+            -- Tarots
+                -- Hanged Man
+                -- Magician
+                -- Empress
+                -- Hierophant
+                -- Lovers
+                -- Chariot
+                -- Justice
+                -- Death
+                -- Devil
+                -- Tower
+                -- Star
+                -- Sun
+                -- Moon
+                -- World
+            -- Spectral
+                -- Familairal
+                -- Grim
+                -- Incantation
+                -- Criptid
+                -- Aura
+                -- Sigil
+                -- Ouiji
+                -- Emilate
+                -- Deja Vu
+                -- Trance
+                -- Medium
+                -- Talisman
+
+-- G.jokers.cards
+    -- Check for the following states
+        -- Player loads up a game from main menu to a round (this is how throwback joker is unlocked)
+        -- Buying one
+        -- Selling one
+        -- Buying a bufoon booster (and not pressing "skip")
+        -- Consumables
+            -- Tarots
+                -- Wheel
+                -- Judgement
+            -- Spectral
+                -- Soul
+                -- Wraith
+                -- Ectoplasm
+                -- Ankhe
+                -- Hex
